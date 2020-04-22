311b70d3f27e5df7d88b4901775cb92c5401b9e1
rocketromero <36490856+rocketromero@users.noreply.github.com>
2020-04-21 22:58:10 -0600

    Tweets are now up on site under candidate. Bug on appended tweets to trump

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,6 +1,42 @@
 from flask import Flask, render_template
+#from shelljob import proc
+import csv
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open('/Users/Rocket/Desktop/BigData2020/data/B_BD2020_2020-04-21 21:24:19.572361.csv') as trump_csv_file:
+        trump_data = csv.reader(trump_csv_file, delimiter=',')
+        first_line = True
+        trump_tweets = []
+        for row in trump_data:
+            if not first_line:
+                trump_tweets.append({
+                    "id": row[0],
+                    "user_location": row[1],
+                    "text": row[2]
+                })
+            else:
+                first_line = False
+        trump_csv_file.close()
+# Bug: Under trump we get the trump tweets + the biden tweets at the end
+#cont: Under biden we just get the trump tweets Need to fix this
+
+    with open('/Users/Rocket/Desktop/BigData2020/data/D_BD2020_2020-04-21 21:24:19.572361.csv') as biden_csv_file:
+        biden_data = csv.reader(biden_csv_file, delimiter=',')
+        first_line1 = True
+        biden_tweets = []
+        for row in biden_data:
+            if not first_line1:
+                biden_tweets.append({
+                    "id": row[0],
+                    "user_location": row[1],
+                    "text": row[2]
+                })
+            else:
+                first_line1 = False
+        biden_csv_file.close()
+
+    return render_template('index.html', trump_tweets=trump_tweets, biden_tweets=biden_tweets)
+
--- streaming_data.py
+++ streaming_data.py
@@ -3,18 +3,21 @@ import time
 import private
 import json
 import dataset
-from datetime import datetime 
+from datetime import datetime
 from sqlalchemy.exc import ProgrammingError
 from textblob import TextBlob
 from datafreeze import freeze
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
-
-
-# --------------------------------- Initilization ----------------------------- # 
+# --------------------------------- Initilization ----------------------------- #
 
 csvFileName = "BD2020_" + str(datetime.now())
 
-terms = ["Donald Trump", "Bernie Sanders",  "Bill Clinton"]
+donnnyBoyTerms = ["Donald Trump", "Trump", "DonaldTrump"]
+
+msLewinskyTerms = ["Joe Biden", "Biden", "JoeBiden"]
 
 sqlTableName = csvFileName + "_table"
 
@@ -22,111 +25,142 @@ sqlConnectionString = "sqlite:///tweets.db"
 
 db = dataset.connect(sqlConnectionString)
 
-streamTimeLimit = 20
+streamTimeLimit = 30
 
 
-
-# --------------------------------- Stream Listener ----------------------------- # 
+# --------------------------------- Stream Listener ----------------------------- #
 
 class StreamListener(tweepy.StreamListener):
-	
-	def __init__(self, time_limit=streamTimeLimit):
-		self.start_time = time.time()
-		self.limit = time_limit
-		self.count = 0
-		super(StreamListener, self).__init__()
-	
-	# each time we get a tweet this method is called
-	def on_status(self, status):
-		self.count +=1
-		if (time.time() - self.start_time) < self.limit:
 
-			# we dont care about retweets
-			if status.retweeted:
-				return			
-			
-			# json fields returned that we chose to add to our database, can be adjusted later
-			loc = status.user.location
-			text = status.text
-			name = status.user.screen_name
-			followers = status.user.followers_count
-			id_str = status.id_str
-			created = status.created_at
-			retweets = status.retweet_count
-			blob = TextBlob(text)
-			sent = blob.sentiment
-			time_stamp = datetime.now()
+    def __init__(self, time_limit, term):
+        self.start_time = time.time()
+        self.limit = time_limit
+        self.count = 0
+        self.tracked_term = term
+        super(StreamListener, self).__init__()
 
-			# need location since we are adding to a map
-			if loc is None:
-				return
+    # each time we get a tweet this method is called
+    def on_status(self, status):
+        self.count += 1
+        if (time.time() - self.start_time) < self.limit and self.count < 50:
 
-			# need positive tweets otherwise our data holds no significance
-			if sent.polarity < 0:
-				return
+            # we dont care about retweets
+            if status.retweeted:
+                return
 
-			print("Tweet from: " + str(loc))
-			addItem(db, loc, text, name, followers, id_str, created, retweets, blob, sent,time_stamp)
-			return True
+            # json fields returned that we chose to add to our database, can be adjusted later
+            loc = status.user.location
+            text = status.text
+            name = status.user.screen_name
+            followers = status.user.followers_count
+            id_str = status.id_str
+            created = status.created_at
+            retweets = status.retweet_count
+            blob = TextBlob(text)
+            sent = blob.sentiment.polarity
+            time_stamp = datetime.now()
 
-		else:
-			exportCSV()
-			return False
+            # need location since we are adding to a map
+            if loc is None:
+                return
 
-		
-	def on_error(self, status_code):
-		if status_code == 420:
-            #returning False in on_data disconnects the stream
-			return False
+            print("Tweet sent: " + str(sent))
+            addItem(db, loc, text, name, followers, id_str, created, retweets, blob, sent, time_stamp)
+            return True
 
+        else:
+            exportCSV(self.tracked_term)
+            return False
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            return False
 
 
 # -------------------------------------- Database Metzhods ----------------------------------- #
 
 # Dataset is a lightweight database that lets us organzie/extract our stream data efficiently
 def addItem(db, loc, text, name, followers, id_str, created, retweets, blob, sent, time_stamp):
-	
-	table = db[sqlTableName]
-	try:
-		table.insert(dict(
-			user_location=loc,
-			text=text,
-			user_name=name,
-			user_followers=followers,
-			id_str=id_str,
-			created=created,
-			retweet_count=retweets,
-			polarity=sent.polarity,
-			subjectivity=sent.subjectivity,
-			time = time_stamp
-		))
-	except ProgrammingError as err:
-		print(err)
+    table = db[sqlTableName]
+    try:
+        table.insert(dict(
+            user_location=loc,
+            text=text,
+            user_name=name,
+            user_followers=followers,
+            id_str=id_str,
+            created=created,
+            retweet_count=retweets,
+            polarity=sent,
+            time=time_stamp
+        ))
+    except ProgrammingError as err:
+        print(err)
+
 
 # Method to write to unique csv file that we will later query
-def exportCSV():
-	print("Export data to csv: " + csvFileName)
-	db = dataset.connect(sqlConnectionString)
-	result = db[sqlTableName].all()
-	freeze(result, format='csv', filename= "data/" + csvFileName)
+def exportCSV(canidateName):
+    print("Export data to csv: " + canidateName + "_" + csvFileName)
+    db = dataset.connect(sqlConnectionString)
+    result = db[sqlTableName].all()
+    newFileName = "data/" + canidateName + "_" + csvFileName + ".csv"
+    freeze(result, format='csv', filename=newFileName)
+
+    if canidateName == "B":
+        bucket = "bd2020billtweets"
+    else:
+        bucket = "bd2020donaldtweets"
+    upload_file(newFileName, bucket)
+
+
+# -------------------------------------- AWS Metzhods ----------------------------------- #
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3', aws_access_key_id=private.AWS_ACCESS_KEY,
+                             aws_secret_access_key=private.AWS_SECRET_KEY)
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
 # ------------------------------------ Main -------------------------------- #
 
 def main():
+    # create instance of the listener
+    auth = tweepy.OAuthHandler(private.TWITTER_APP_KEY, private.TWITTER_APP_SECRET)
+    auth.set_access_token(private.TWITTER_KEY, private.TWITTER_SECRET)
+    api = tweepy.API(auth)
+    print("Authorized with Twitter API")
 
-	# create instance of the listener
-	auth = tweepy.OAuthHandler(private.TWITTER_APP_KEY, private.TWITTER_APP_SECRET)
-	auth.set_access_token(private.TWITTER_KEY, private.TWITTER_SECRET)
-	api = tweepy.API(auth)
-	print("Authorized with Twitter API")
+    print("Streaming Trump tweets")
+    stream_listener_Donny = StreamListener(streamTimeLimit, "D")
+    stream = tweepy.Stream(auth=api.auth, listener=stream_listener_Donny)
+    stream.filter(track=donnnyBoyTerms)
+    print("Streamed " + str(stream_listener_Donny.count) + " tweets")
 
-	print("Streaming tweets")
-	stream_listener = StreamListener()
-	stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-	stream.filter(track = terms)
-	print("Streamed " + str(stream_listener.count) + " tweets")
-	
+    print("Streaming Biden tweets")
+    stream_listener_Biden = StreamListener(streamTimeLimit, "B")
+    stream = tweepy.Stream(auth=api.auth, listener=stream_listener_Biden)
+    stream.filter(track=msLewinskyTerms)
+    print("Streamed " + str(stream_listener_Biden.count) + " tweets")
+
 
 if __name__ == "__main__":
-	main()
+    main()
